5075a237b4da7d723a9d1375ffcf826ae2f410e0
wwombat <willful.wombat@gmail.com>
2017-12-17 20:10:02 -0500

    clean up directory structure
    Move everything except the start script up into a 'resources'
    folder.

--- present/start.sh
+++ present/start.sh
@@ -3,5 +3,13 @@
 # Adventuring requires python3
 PYTHON=$(which python3)
 
+rc=$?
+if [ ! $rc -eq 0 ]; then
+    echo "error: python3 is needed for adventuring!"
+    exit -1
+fi
+
+cd resources
+
 # Adventures must be performed in a clean environment
 env -i bash --noprofile --init-file <(echo "PYTHON=${PYTHON}; source ./present.cfg")
